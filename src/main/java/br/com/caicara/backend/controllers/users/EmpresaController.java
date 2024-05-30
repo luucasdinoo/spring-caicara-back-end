3023ab9494aec17a75f17981119dffa60347776a
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-29 21:03:00 -0300

    Métodos getAllEmpresas getEmpresaById e updateEmpresa

--- src/main/java/br/com/caicara/backend/controllers/users/EmpresaController.java
+++ src/main/java/br/com/caicara/backend/controllers/users/EmpresaController.java
@@ -2,6 +2,8 @@ package br.com.caicara.backend.controllers.users;
 
 import br.com.caicara.backend.model.dto.users.EmpresaCreateDto;
 import br.com.caicara.backend.model.dto.users.EmpresaResponseDto;
+import br.com.caicara.backend.model.dto.users.EmpresaUpdateDto;
+import br.com.caicara.backend.model.dto.users.RibeirinhoUpdateDto;
 import br.com.caicara.backend.model.entities.mapper.EmpresaMapper;
 import br.com.caicara.backend.model.entities.users.Empresa;
 import br.com.caicara.backend.model.jwt.JwtUserDetails;
@@ -14,6 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/empresa")
@@ -37,5 +42,26 @@ public class EmpresaController {
     public ResponseEntity<EmpresaResponseDto> getDetalhes(@AuthenticationPrincipal JwtUserDetails userDetails){
         Empresa emp = empresaService.getUserById(userDetails.getId());
         return ResponseEntity.ok(EmpresaMapper.toDto(emp));
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADM')")
+    public ResponseEntity<List<EmpresaResponseDto>> getAllEmpresas(){
+        List<EmpresaResponseDto> list = EmpresaMapper.toListEmpresa(empresaService.getAllEmpresas());
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADM')")
+    public ResponseEntity<EmpresaResponseDto> getEmpresaById(@PathVariable UUID id){
+        Empresa emp = empresaService.getEmpresaById(id);
+        return ResponseEntity.ok(EmpresaMapper.toDto(emp));
+    }
+
+    @PatchMapping("/update/{id}")
+    @PreAuthorize("hasRole('EMPRESA')")
+    public ResponseEntity<Void> updateEmpresa(@RequestBody @Valid EmpresaUpdateDto dto, @PathVariable UUID id){
+        empresaService.updateEmpresa(id, dto);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/com/caicara/backend/model/services/users/EmpresaService.java
+++ src/main/java/br/com/caicara/backend/model/services/users/EmpresaService.java
@@ -1,11 +1,16 @@
 package br.com.caicara.backend.model.services.users;
 
+import br.com.caicara.backend.model.dto.users.EmpresaUpdateDto;
+import br.com.caicara.backend.model.dto.users.RibeirinhoUpdateDto;
 import br.com.caicara.backend.model.entities.users.Empresa;
+import br.com.caicara.backend.model.entities.users.Ribeirinho;
 import br.com.caicara.backend.model.repositories.users.EmpresaRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -13,6 +18,7 @@ import java.util.UUID;
 public class EmpresaService {
 
     private final EmpresaRepository empresaRepository;
+    private final ModelMapper modelMapper;
 
     @Transactional
     public Empresa createEmpresa(Empresa emp){
@@ -22,5 +28,23 @@ public class EmpresaService {
     @Transactional(readOnly = true)
     public Empresa getUserById(UUID id) {
         return empresaRepository.findByUsersId(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Empresa> getAllEmpresas() {
+        return empresaRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+
+    public Empresa getEmpresaById(UUID id) {
+        return empresaRepository.findById(id).orElseThrow((() -> new RuntimeException("Id não encontrado")));
+    }
+
+    @Transactional
+    public void updateEmpresa(UUID id, EmpresaUpdateDto dto) {
+        Empresa emp = empresaRepository.findById(id).orElseThrow(()-> new RuntimeException("Id não encontrado"));
+        modelMapper.map(dto, emp);
+        empresaRepository.save(emp);
     }
 }
